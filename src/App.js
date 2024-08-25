bbae035589a3833d849cc2cd1789cbed983e7af5
Dharmishtha A <dharmishtha9879@gmail.com>
2024-08-25 20:23:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+  const [editingIndex, setEditingIndex] = useState(-1);
+  const [error, setError] = useState("");
+
+  const handleAddTodo = () => {
+    if (newTodo.trim() === "") {
+      setError("Todo cannot be empty.");
+      return;
+    }
+    if (newTodo.length > 32) {
+      setError("cant exceed 32 characters.");
+      return;
+    }
+    setError("");
+    if (editingIndex >= 0) {
+      const updatedTodos = todos.map((todo, index) =>
+        index === editingIndex ? newTodo : todo
+      );
+      setTodos(updatedTodos);
+      setEditingIndex(-1);
+    } else {
+      setTodos([...todos, newTodo]);
+    }
+    setNewTodo("");
+  };
+
+  const handleEditTodo = (index) => {
+    setNewTodo(todos[index]);
+    setEditingIndex(index);
+    setError("")
+  };
+
+  const handleDeleteTodo = (index) => {
+    const updatedTodos = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodos);
+  };
+  const handleChange = (e) => {
+    setNewTodo(e.target.value)
+    setError("")
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <div>
+        <input
+          type="text"
+          value={newTodo}
+          onChange={ handleChange }
+          placeholder="Enter a todo"
+        />
+        <button onClick={handleAddTodo}>
+          {editingIndex >= 0 ? "Update" : "Add"}
+        </button>
+      </div>
+      {error && <p className="error">{error}</p>}
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            <p>{todo}</p>
+            <div>
+            <button onClick={() => handleEditTodo(index)}>Edit</button>
+            <button onClick={() => handleDeleteTodo(index)}>Delete</button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
 export default App;
+
